8f745a373fe1fac40ec60c6c8e6a55af812a4cb6
Emre Turhan <emre.turhan@ug.bilkent.edu.tr>
2023-12-15 22:50:02 +0300

    Logging system added

--- backend/fastApiProject/db_connection/firebase_auth.py
+++ backend/fastApiProject/db_connection/firebase_auth.py
@@ -1,11 +1,12 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import logging
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 def connect_db():
     cred = credentials.Certificate("fastApiProject/db_connection/credentials.json")
     firebase_admin.initialize_app(cred)
+    logger.info("Firebase connection established")
     return firestore.client()
-
-
-
