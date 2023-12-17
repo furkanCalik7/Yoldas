56ada9a808a2f733391944e98c8c24e728115d0e
furkanguzelant <72248390+furkanguzelant@users.noreply.github.com>
2023-12-17 03:43:01 +0300

    user data added

---
+++ frontend/lib/models/user_data.dart
@@ -0,0 +1,9 @@
+class UserData {
+
+  final String name;
+  final String mail;
+  final String password;
+  final String phoneNumber;
+
+  UserData({this.name = "", this.mail = "", required this.password, required this.phoneNumber});
+}
