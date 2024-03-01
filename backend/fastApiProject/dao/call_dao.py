f94a1886d12aaa66bbdcc4a88241944fc3efd2bd
Emre Turhan <emre.turhan@ug.bilkent.edu.tr>
2024-03-01 04:38:05 +0300

    build-and-deploy.yml created

--- backend/fastApiProject/dao/call_dao.py
+++ backend/fastApiProject/dao/call_dao.py
@@ -3,7 +3,7 @@ from ..models.entity_models import Call
 
 db = firebase_auth.connect_db()
 
-
+#try for build
 def register_call(call: Call):
     call_col_ref = db.collection("CallCollection").add(call.model_dump())
     return {"call": call_col_ref}
