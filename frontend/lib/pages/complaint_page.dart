ec37cc0bacbda3285b02167e8b370890b2d92880
Can-Yilmaz <yilmaz_can@ug.bilkent.edu.tr>
2024-03-27 18:27:17 +0300

    Complaint Page Debug

--- frontend/lib/pages/complaint_page.dart
+++ frontend/lib/pages/complaint_page.dart
@@ -19,53 +19,58 @@ class ComplaintPage extends StatelessWidget {
           child: Center(
             child: Padding(
               padding: const EdgeInsets.all(20.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Şikayetinizi buraya yazabilirsiniz.",
-                    style: TextStyle(
-                      fontSize: 20,
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Şikayetinizi buraya yazabilirsiniz.",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white70,
+                    const SizedBox(
+                      height: 20,
                     ),
-                    child: TextField(
-                      maxLines: 10,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(0),
+                    Container(
+                      decoration: const BoxDecoration(
+                        color: Colors.white70,
+                      ),
+                      child: TextField(
+                        maxLines: 10,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(0),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ButtonMain(
-                        text: "Gönder",
-                        width: MediaQuery.of(context).size.width * 0.4,
-                        action: () {},
-                      ),
-                      const Spacer(),
-                      ButtonMain(
-                        text: "İptal",
-                        width: MediaQuery.of(context).size.width * 0.4,
-                        action: () {
-                          Navigator.pop(context);
-                        },
-                      ),
-                    ],
-                  ),
-                ],
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ButtonMain(
+                          text: "Gönder",
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          action: () {},
+                        ),
+                        const Spacer(),
+                        ButtonMain(
+                          text: "İptal",
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          action: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 100,
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
