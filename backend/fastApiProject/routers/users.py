e2a253cfffdd878fba8a9675331324755152a357
Berkay Çalmaz <berkay.calmaz@ug.bilkent.edu.tr>
2024-05-05 14:40:40 +0300

    YLS-181 refactored user endpoints.

--- backend/fastApiProject/routers/users.py
+++ backend/fastApiProject/routers/users.py
@@ -13,6 +13,50 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 
+@router.post("/register")
+async def register_user(user: entity_models.User):
+    logger.info(f"register_user with user {user} called")
+    # If there is a missing or wrong input, it returns appropriate response.
+    return user_manager.register_user(user)
+
+
+@router.post("/login")
+async def login_for_access_token(
+        form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+):
+    return user_manager.login_for_access_token(form_data)
+
+
+@router.put("/update/{user_id}")
+async def update_user(update_user_request: request_models.UpdateUserRequest,
+                      current_user: Annotated[entity_models.User, Depends(user_manager.get_current_active_user)]):
+    return user_manager.update_user_request(current_user["phone_number"], update_user_request)
+
+
+@router.delete("/delete/{user_id}")
+async def delete_user(user_id: str):
+    logger.info(f"delete_user with user_id {user_id} called")
+    return user_manager.delete_user(user_id)
+
+
+@router.post("/feedback")
+async def send_feedback(feedbackRequest: request_models.FeedbackRequest,
+                        current_user: Annotated[entity_models.User, Depends(user_manager.get_current_active_user)]):
+    return user_manager.send_feedback(feedbackRequest, current_user)
+
+
+@router.post("/complaint")
+async def send_complaint(complaintRequest: request_models.ComplaintRequest,
+                         current_user: Annotated[entity_models.User, Depends(user_manager.get_current_active_user)]):
+    return user_manager.send_complaint(complaintRequest, current_user)
+
+
+@router.get("/get_all_abilities")
+async def get_all_abilities():
+    logger.info(f"get_all_abilities called")
+    return user_manager.get_all_abilities()
+
+
 @router.get("/role")
 async def get_user_role(current_user_role: Annotated[entity_models.User, Depends(user_manager.get_current_user_role)]):
     logger.info(f"get_user_role for user with phoneNumber {entity_models.User.uid} called")
@@ -35,50 +79,3 @@ async def get_user_by_matching_abilities(abilities: str):
 async def get_user_by_rating_average(low: int, high: int):
     logger.info(f"get_user_by_feedback_average for rating in range {low} and {high} called")
     return user_manager.get_user_by_rating_average(low, high)
-
-
-@router.post("/feedback")
-async def send_feedback(feedbackRequest: request_models.FeedbackRequest,
-                        current_user: Annotated[entity_models.User, Depends(user_manager.get_current_active_user)]):
-    return user_manager.send_feedback(feedbackRequest, current_user)
-
-
-@router.post("/complaint")
-async def send_complaint(complaintRequest: request_models.ComplaintRequest,
-                         current_user: Annotated[entity_models.User, Depends(user_manager.get_current_active_user)]):
-    return user_manager.send_complaint(complaintRequest, current_user)
-
-
-@router.get("/get_all_abilities")
-async def get_all_abilities():
-    logger.info(f"get_all_abilities called")
-    return user_manager.get_all_abilities()
-
-
-@router.post("/register")
-async def register_user(user: entity_models.User):
-    logger.info(f"register_user with user {user} called")
-    # If there is a missing or wrong input, it returns appropriate response.
-    return user_manager.register_user(user)
-
-@router.delete("/delete/{user_id}")
-async def delete_user(user_id: str):
-    logger.info(f"delete_user with user_id {user_id} called")
-    return user_manager.delete_user(user_id)
-
-@router.post("/login")
-async def login_for_access_token(
-        form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
-):
-    return user_manager.login_for_access_token(form_data)
-
-
-@router.get("/get_all_abilities}")
-async def get_all_abilities():
-    logger.info(f"get_all_abilities called")
-    return user_manager.get_all_abilities()
-
-@router.put("/update/{user_id}")
-async def update_user(update_user_request: request_models.UpdateUserRequest,
-                      current_user: Annotated[entity_models.User, Depends(user_manager.get_current_active_user)]):
-    return user_manager.update_user_request(current_user["phone_number"], update_user_request)
